29d6ef188bebf69b5ac9d6187ffe5c67cae7f6de
stilobique <stilobique@users.noreply.github.com>
2017-06-23 14:43:49 +0200

    Add a basic way to generate all data

--- Build Light UE4.py
+++ Build Light UE4.py
@@ -1,11 +1,82 @@
 import os
+import perforce
+import tkinter
 
-path_ue4 = r"E:\WORKS\Perforce\EpicGames\UE4-QA\Engine\Binaries\Win64\UE4Editor.exe"
-path_project = r"E:\WORKS\Perforce\ProVolley\UnrealProjects\ProVolley\ProVolley.uproject"
+# -----------------------------
+# Generate all data needs to know enviroment we want build, the stadium
+# name, suffix name and -by deduction the path
+# -----------------------------
+lvl_root = '//ProVolley/UnrealProjects/ProVolley/Content/Scenes/'
+lvl_suffix = [
+    # Gymnasium
+    'GYM01',
+    'GYM02',
+    # Stadium
+    'STA01',
+    'STA02',
+    'STA03',
+    'STA04',
+    'STA05',
+    'STA06',
+    'STA07',
+    'STA08',
+    'STA09',
+    # # Training Courts
+    # 'TC01',
+    # 'TC02'
+]
+lvl_folder = [
+    # Gymnasium
+    'SanJuanTheater',
+    'MittelbrunnZentrum',
+    # Stadium
+    'UmmDharbStadium',
+    'ManzoVBArena',
+    'BanKhaemSporthall',
+    'HosojimiCenter',
+    'CharlesFrabetStadium',
+    'JalbarosCenterArena',
+    'CuapixcoEsteColegio',
+    'AbramCenterStadium',
+    'PretovkaClubStadion',
+    # Training Courts
+    # 'RoyalStratfordGymnasium',
+    # 'MartinSherpardHall'
+]
 
-path_ue4 path_project -run=resavepackages -buildlighting \
-                           -AllowCommandletRendering -AutoCheckOutPackages \
-                           -FILE=STA02.umap
+
+# -----------------------------
+# Connect to perfoce to check all map (and lvl ussat), check if i can't move
+#  or move to next
+# -----------------------------
+p4 = perforce.connect()
+for i in range(len(lvl_suffix)):
+    lvl_name = lvl_suffix[i]
+    lvl_end = lvl_folder[i]
+    map = lvl_root + lvl_name + '_' + lvl_end + '/' + lvl_name + '.umap'
+
+    i = +1
+    # revision = p4.ls(map)
+    print(map)
+
+# fenetre = tkinter.Tk()
+#
+# for lvl_name in levels:
+#     suffix = lvl_name
+#     name = 'button_' + lvl_name
+#     name = tkinter.Checkbutton(fenetre, text=lvl_name)
+#     name.pack()
+#
+#
+# fenetre.mainloop()
+
+
+# path_ue4 = r"E:\WORKS\Perforce\EpicGames\UE4-QA\Engine\Binaries\Win64\UE4Editor.exe"
+# path_project = r"E:\WORKS\Perforce\ProVolley\UnrealProjects\ProVolley\ProVolley.uproject"
+#
+# path_ue4 path_project -run=resavepackages -buildlighting \
+#                            -AllowCommandletRendering -AutoCheckOutPackages \
+#                            -FILE=STA02.umap
 
 
 
--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-python.exe "E:\WORKS\Git\Personal Scripts\personal-script\update_blender_2_8.py"
+python.exe "E:\WORKS\Git\Personal Scripts\personal-script\Build Light UE4.py"
 
 pause
