38766c851cafefb9f11d68b9ff9c8db5522fe3bd
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-08-13 22:00:37 +0200

    Refactoring repo, clean unused file ; and split the tools "BacthBuildLightUE4"

--- _Build UE4 Light.bat
+++
@@ -1,3 +0,0 @@
-E:\WORKS\Perforce\EpicGames\UE4-QA\Engine\Binaries\Win64\UE4Editor.exe "E:\WORKS\Perforce\ProVolley\UnrealProjects\ProVolley\ProVolley.uproject" -run=resavepackages -buildlighting -AllowCommandletRendering -AutoCheckOutPackages -FILE=STA02.umap
-
-pause
--- run.bat
+++
@@ -1,3 +0,0 @@
-python.exe "E:\WORKS\Git\Personal Scripts\personal-script\Build Light UE4.py"
-
-pause
